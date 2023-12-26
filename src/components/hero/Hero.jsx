d5bff71d91c8fb8a931a924650d6bd4244fe0fd0
unusdv <asilbekdilboboev77@gmail.com>
2023-12-26 08:10:07 -1100

    Created Project

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Hero.scss"
+import Nav from '../../layout/nav/Nav'
+const Hero = () => {
+  return (
+    <header>
+        <Nav/>
+        <div className="hero__wrapper">
+                <div className="hero__content">
+
+                </div>
+                <div className="hero__image">
+                  
+                </div>
+        </div>
+    </header>
+  )
+}
+
+export default Hero
---
+++ src/layout/nav/Nav.jsx
@@ -0,0 +1,27 @@
+import "./Nav.scss"
+import Logo from "../../assets/images/logo.svg"
+import { NavLink } from "react-router-dom"
+
+const Nav = () => {
+  return (
+    <nav>
+      <div className="nav__wrapper">
+        <div className="nav__logo">
+          <img src={Logo} alt="Logo of Website" />
+        </div>
+        <ul className="nav__menu">
+          <li><NavLink className={({isActive}) => isActive ? "nav__link nav__link--active" : "nav__link"} to={"/"}>Home</NavLink></li>
+          <li><NavLink className={({isActive}) => isActive ? "nav__link nav__link--active" : "nav__link"} to={"about"}>About</NavLink></li>
+          <li><NavLink className={({isActive}) => isActive ? "nav__link nav__link--active" : "nav__link"} to={"article"}>Article</NavLink></li>
+          <li><NavLink className={({isActive}) => isActive ? "nav__link nav__link--active" : "nav__link"} to={"service"}>Services</NavLink></li>
+          <li><NavLink className={({isActive}) => isActive ? "nav__link nav__link--active" : "nav__link"} to={"service"}>Contact Us</NavLink></li>
+        </ul>
+        <div className="nav__action">
+          <button>Get Started</button>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Nav
