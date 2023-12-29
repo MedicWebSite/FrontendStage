e3c4f0eedc4f4f29fc9cce6e91722781cea62110
mehrojalieev <aliyevmehrojbek0@gmail.com>
2023-12-29 23:52:24 +0500

    Test

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from '../../components/hero/Hero'
 import Meeting from '../../components/meeting/Meeting'
+import OurSerive from '../../components/our-service/OurSerive'
 
 const Home = () => {
   return (
     <div>
         <Hero/>
+        <OurSerive/>
         <Meeting/>
     </div>
   )
