47d337172a6edcebd2db3aa026cdb6ab96fc19c6
Web-King007 <abbosxoliqberdiyev622@gmail.com>
2024-01-31 11:36:36 +0500

    create login

--- src/routes/RoutController.jsx
+++ src/routes/RoutController.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
 import Home from '../pages/home/Home'
-import Login from './login/Login'
-import SignUp from './sign-up/SignUp'
-import Auth from '../pages/auth/Auth'
+import Login from './auth/login/Login'
+import SignUp from './auth/sign-up/SignUp'
+import Auth from '../routes/auth/Auth'
+import { Route, Routes } from 'react-router-dom'
 
 const RoutController = () => {
   return (
